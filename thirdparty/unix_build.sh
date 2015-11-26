1b6b0dc60a9626bf7773724c4cba66eb6a5afb24
Siwei Xu <xusiwei1236@163.com>
2015-11-27 02:29:44 +0800

    prepare to handle enviroment compatible

--- thirdparty/unix_build.sh
+++ thirdparty/unix_build.sh
@@ -1,19 +1,21 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-cd ./curl-7.45.0/
-./configure
-make
-#sudo make install
-cd ..
-
-cd ./apr-1.5.2/
-./configure
-make
-#sudo make install
-cd ..
-
-cd ./apr-util-1.5.4/
-./configure --with-apr=../apr-1.5.2/
-make
-#sudo make install
-cd ..
+if [ "$1" == "curl" ]; then
+    echo build CURL
+    cd ./curl-7.45.0/
+    ./configure
+    make
+    cd ..
+elif [ "$1" == "apr" ]; then
+    echo build APR
+    cd ./apr-1.5.2/
+    ./configure
+    make
+    cd ..
+elif [ "$1" == "apu" ]; then
+    echo build APR-UTIL
+    cd ./apr-util-1.5.4/
+    ./configure --with-apr=../apr-1.5.2/
+    make
+    cd ..
+fi
