22e6709dc772b8c769595ae285290eed7d7d4376
Siwei Xu <xusiwei1236@163.com>
2015-11-27 21:44:35 +0800

    compatible with Windows(MSVC)

--- thirdparty/win_build.bat
+++ thirdparty/win_build.bat
@@ -1,1 +1,26 @@
+@echo off
 
+rem other version, change follow path:
+set varset="C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
+
+echo set env %varset% ...
+
+if "%1" == "curl" (
+	echo build CURL
+	cd curl-7.45.0\winbuild
+	%varset%
+	nmake /f Makefile.vc mode=static
+	cd ..
+) else if "%1" == "apr" (
+	echo build APR
+	cd apr-1.5.2
+	%varset%
+	nmake /f apr.mak
+	cd ..
+) else if "%1" == "apu" (
+	echo build APR-UTIL
+	cd apr-util-1.5.4
+	%varset%
+	nmake /f aprutil.mak
+	cd ..
+)
